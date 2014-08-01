8591d2bc7d1341d1d67056c8a9062dce96f8363a
DraygoKorvan <draygokorvan@ps-roughriders.com>
2014-08-01 00:19:35 -0500

    -final fixes for 2.2.4

--- Core.cs
+++ Core.cs
@@ -39,6 +39,10 @@ namespace SEDropship
 	{
 		private int m_slowDownDistance = 250;
 		private bool m_anyAsteroid = true;
+		private int m_resolution = 1000;
+		private float m_slowSpeed = 5.0F;
+		private float m_startSpeed = 104.4F;
+		private int m_countdown = 10;
 		public int slowDownDistance
 		{
 			get { return m_slowDownDistance; }
@@ -49,6 +53,26 @@ namespace SEDropship
 		{
 			get { return m_anyAsteroid; }
 			set { m_anyAsteroid = value; }
+		}
+		public int resolution
+		{
+			get { return m_resolution; }
+			set { if (value > 0 ) m_resolution = value; }
+		}
+		public float slowSpeed
+		{
+			get { return m_slowSpeed; }
+			set { if (value >= 0F) m_slowSpeed = value; }
+		}
+		public float startSpeed
+		{
+			get { return m_startSpeed; }
+			set { if(value >= 0F) m_startSpeed = value; }
+		}
+		public int countdown
+		{
+			get { return m_countdown; }
+			set { if(value >= 0) m_countdown = value; }
 		}
 	}
 	public class SEDropship : PluginBase, IChatEventHandler
@@ -115,22 +139,72 @@ namespace SEDropship
 			set { settings.anyAsteroid = value; }
 		}
 
+		[Category("SE Dropship")]
+		[Description("Resolution settings, 1 second = 1000")]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		public int resolution
+		{
+			get { return settings.resolution; }
+			set { settings.resolution = value; }
+		}
+
+		[Category("SE Dropship")]
+		[Description("How fast the ship goes when it is near its target")]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		public float slowSpeed
+		{
+			get { return settings.slowSpeed; }
+			set { settings.slowSpeed = value; }
+		}
+
+		[Category("SE Dropship")]
+		[Description("Initial dropship speed")]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		public float startSpeed
+		{
+			get { return settings.startSpeed; }
+			set { settings.startSpeed = value; }
+		}
+
+		[Category("SE Dropship")]
+		[Description("Distance from target before dropship slows down")]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		public int slowDownDistance
+		{
+			get { return settings.slowDownDistance; }
+			set { settings.slowDownDistance = value; }
+		}
+		[Category("SE Dropship")]
+		[Description("Countdown, sets amount of seconds for insertion sequence to start")]
+		[Browsable(true)]
+		[ReadOnly(false)]
+		public int countdown
+		{
+			get { return settings.countdown; }
+			set { settings.countdown = value; }
+		}		
+
 		#endregion
 
 		#region "Methods"
 
 		private void mainloop()
 		{
-			Thread.Sleep(1000);
+			Thread.Sleep(resolution);
 			List<CubeGridEntity> ignore = SectorObjectManager.Instance.GetTypedInternalData<CubeGridEntity>();
 			while(m_running)
 			{
 				try
 				{
-					Thread.Sleep(1000); //update the resolution to 1 second
+					Thread.Sleep(resolution); //update the resolution to 1 second
 					List<CubeGridEntity> huntlist = SectorObjectManager.Instance.GetTypedInternalData<CubeGridEntity>();
 					foreach (CubeGridEntity grid in huntlist)
 					{
+						Thread.Yield();//yield processing to other things
 						if (grid.IsLoading) continue;
 						if (ignore.Exists(x => x.EntityId == grid.EntityId))
 						{
@@ -138,18 +212,28 @@ namespace SEDropship
 						}
 						ignore.Add(grid);
 						if (ServerNetworkManager.Instance.GetConnectedPlayers().Count == 0) continue;
+						long  tempowner = 0;
+						bool found = false;
+						CockpitEntity cockpit = null;
 						foreach (CubeBlockEntity cubein in grid.CubeBlocks)
 						{
+							if (cubein.Owner != 0) tempowner = cubein.Owner;
+							
 							if (cubein is CockpitEntity)
 							{
-								CockpitEntity cockpit = (CockpitEntity)cubein;
+								cockpit = (CockpitEntity)cubein;
 								if (cockpit.CustomName == "Dropship")
 								{
-									Thread dropstep = new Thread(() => doDrop(grid));
-									dropstep.Priority = ThreadPriority.BelowNormal;
-									dropstep.Start();
+									found = true;
+									
 								}
 							}
+						}
+						if(found && cockpit != null)
+						{
+							Thread dropstep = new Thread(() => doDrop(grid, cockpit, tempowner));
+							dropstep.Priority = ThreadPriority.BelowNormal;
+							dropstep.Start();
 						}
 					}
 				}
@@ -164,8 +248,7 @@ namespace SEDropship
 		{
 
 			Vector3 dirToTarget = Vector3.Normalize(targetOrigin - spawnOrigin);
-			Vector3 targetVelOrth =
-			Vector3.Dot(targetVel, dirToTarget) * dirToTarget;
+			Vector3 targetVelOrth = Vector3.Dot(targetVel, dirToTarget) * dirToTarget;
 			Vector3 targetVelTang = targetVel - targetVelOrth;
 			Vector3 shotVelTang = targetVelTang;
 			float shotVelSpeed = shotVelTang.Length();
@@ -180,13 +263,13 @@ namespace SEDropship
 				return shotVelOrth + shotVelTang;
 			}
 		}
-		public void doDrop(CubeGridEntity grid)
+		public void doDrop(CubeGridEntity grid, CockpitEntity seat, long Owner)
 		{
 			//find target
 			List<VoxelMap> asteroids = SectorObjectManager.Instance.GetTypedInternalData<VoxelMap>();
 			//int asteroidcount = asteroids.Count();
 			VoxelMap asteroid = asteroids.First();
-			//pick a random asteroid if desired:
+			//pick a random asteroid if desired: going to change this up later
 			if(anyAsteroid)
 			{
 				VoxelMap roid = asteroids.ElementAt(m_gen.Next(asteroids.Count));
@@ -195,22 +278,56 @@ namespace SEDropship
 			}
 
 			Vector3Wrapper target = asteroid.Position;
-			
-
+			List<ulong> steamlist = ServerNetworkManager.Instance.GetConnectedPlayers();
+			ulong steamid = 0;
+			foreach( ulong steam_id in steamlist)
+			{
+				List<long> playerids =  PlayerMap.Instance.GetPlayerIdsFromSteamId(steam_id);
+				//PlayerMap.Instance.get
+				foreach (long playerid in playerids)
+				{
+					if(playerid == Owner)
+						steamid = steam_id;
+				}
+			}
+			ChatManager.Instance.SendPrivateChatMessage(steamid, "Dropship booting up, please stay in your seat for insertion.");
+			Thread.Sleep(1000);
+			ChatManager.Instance.SendPrivateChatMessage(steamid, "If you exited your ship please return to the passenger seat before the countdown begins.");
+			Thread.Sleep(5000);
+			ChatManager.Instance.SendPrivateChatMessage(steamid, "Dropship Sequence Initiated, please remain seated. Exiting your seat will abort automatic insertion.");
+			Thread.Sleep(1000);
+			ChatManager.Instance.SendPrivateChatMessage(steamid, "Beginning Insertion Sequence.");
+			Thread.Sleep(1000);	
+			for ( int count = countdown; count > 0; count--)
+			{
+				ChatManager.Instance.SendPrivateChatMessage(steamid, count.ToString() + ".");
+				if (seat.Pilot == null)
+					break;
+				Thread.Sleep(1000);
+			}
+			if(seat.Pilot != null)
+			{
+				ChatManager.Instance.SendPrivateChatMessage(steamid, "Insertion Sequence initiated.");
+			}
+			else
+			{
+				ChatManager.Instance.SendPrivateChatMessage(steamid, "Insertion Sequence aborted.");
+				return;
+			}
 			target = Vector3.Add(asteroid.Position, new Vector3Wrapper(50, 50, 50));//temp, till we can query asteroid size.
 			Vector3Wrapper position = grid.Position;
-			Vector3Wrapper Vector3Intercept = (Vector3Wrapper)FindInterceptVector(position, 104.4F, target, new Vector3Wrapper(0, 0, 0));
+			Vector3Wrapper Vector3Intercept = (Vector3Wrapper)FindInterceptVector(position, startSpeed, target, new Vector3Wrapper(0, 0, 0));
 			grid.Forward = Vector3.Normalize(Vector3Intercept);
 			grid.LinearVelocity = Vector3Intercept;
-			float timeToCollision = Vector3.Distance(position, Vector3.Subtract(target, Vector3.Multiply(Vector3.Normalize(Vector3Intercept), settings.slowDownDistance))) / Vector3.Distance(new Vector3Wrapper(0, 0, 0), Vector3Intercept);
+			float timeToCollision = Vector3.Distance(position, Vector3.Subtract(target, Vector3.Multiply(Vector3.Normalize(Vector3Intercept), slowDownDistance))) / Vector3.Distance(new Vector3Wrapper(0, 0, 0), Vector3Intercept);
 			Thread.Sleep((int)timeToCollision * 1000);
-			grid.LinearVelocity = Vector3.Multiply(Vector3.Normalize(Vector3Intercept), 5.0F);
+			grid.LinearVelocity = Vector3.Multiply(Vector3.Normalize(Vector3Intercept), slowSpeed);
 		}
 		public void saveXML()
 		{
 
 			XmlSerializer x = new XmlSerializer(typeof(SEDropshipSettings));
-			TextWriter writer = new StreamWriter(Location + "SEMotd-Config.xml");
+			TextWriter writer = new StreamWriter(Location + "SE-Dropship-Settings.xml");
 			x.Serialize(writer, settings);
 			writer.Close();
 
@@ -219,11 +336,11 @@ namespace SEDropship
 		{
 			try
 			{
-				if (File.Exists(Location + "SEMotd-Config.xml") && !defaults)
+				if (File.Exists(Location + "SE-Dropship-Settings.xml") && !defaults)
 				{
 
 					XmlSerializer x = new XmlSerializer(typeof(SEDropshipSettings));
-					TextReader reader = new StreamReader(Location + "SEMotd-Config.xml");
+					TextReader reader = new StreamReader(Location + "SE-Dropship-Settings.xml");
 					SEDropshipSettings obj = (SEDropshipSettings)x.Deserialize(reader);
 	
 					reader.Close();
@@ -311,7 +428,7 @@ namespace SEDropship
 
 		public void OnPlayerJoined(ulong nothing, CharacterEntity character)
 		{
-
+			return;
 		}
 		public void OnPlayerLeft(ulong nothing, CharacterEntity character)
 		{
